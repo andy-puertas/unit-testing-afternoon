0bdbf941c26a588fa90389e8fd50a205cd41cec6
andy-puertas <32721350+andy-puertas@users.noreply.github.com>
2018-07-12 13:38:27 -0600

    initial uploads

---
+++ cart.js
@@ -0,0 +1,18 @@
+const cars = require('./data/cars');
+
+module.exports = {
+  cart: null,
+  total: null,
+
+  addToCart: function() {
+
+  },
+
+  removeFromCart: function() {
+
+  },
+  
+  checkout: function() {
+
+  }
+};
